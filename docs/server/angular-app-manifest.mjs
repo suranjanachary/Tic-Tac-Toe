a98116b5b16adb9c02af0de311d97bd18d7314cf
lopita <lopita.mishra2001@gmail.com>
2025-05-17 01:12:49 +0530

    ic

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -7,13 +7,23 @@ export default {
   routes: [
   {
     "renderMode": 2,
+    "redirectTo": "/Program%20Files/Git/Tic-Tac-Toe/game",
     "route": "/Program%20Files/Git/Tic-Tac-Toe"
+  },
+  {
+    "renderMode": 2,
+    "route": "/Program%20Files/Git/Tic-Tac-Toe/game"
+  },
+  {
+    "renderMode": 2,
+    "redirectTo": "/Program%20Files/Git/Tic-Tac-Toe/game",
+    "route": "/Program%20Files/Git/Tic-Tac-Toe/**"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 525, hash: '6bc99e55e4438bd7737a8f5e10f43a4f4d177b4fadf39b0aca1e020addf955f8', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1038, hash: 'cc18e5159d04682e1895273592171e8e2962df06a63810d792fb161e43575c8b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 525, hash: '631407372441ed8ec6063cdd508cab11176f5bb4201887097710956b4c8a5419', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1038, hash: '984edeeb01d3a070e046f7bda066f8ac87e9adc72cbc37c0d9004d00e07a8eee', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };

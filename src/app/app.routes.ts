51b2be00e9f58ef3aa37ed497e978a03da0fb628
lopita <lopita.mishra2001@gmail.com>
2025-05-17 01:11:00 +0530

    ic

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { GameComponent } from './game/game.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'game',
+        component: GameComponent
+    },
+    {
+        path: '',
+        redirectTo: '/game',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: '/game',
+        pathMatch: 'full'
+    }
+];
